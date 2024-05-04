5f942bda9f28a907dd0e48bd0ade53495b16eb0e
Santiago Sarmiento <73916781+SantiSarmiento@users.noreply.github.com>
2024-05-04 13:52:20 -0300

    se agrego libreria de diseño

--- App.js
+++ App.js
@@ -4,17 +4,21 @@ import { store } from "./src/state/store";
 import Login from "./src/views/auth/Login";
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist'
+import { GluestackUIProvider } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
 
 let persistor = persistStore(store)
 
 const App = () => {
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Login />
-      </PersistGate>
-    </Provider>
+    <GluestackUIProvider config={config}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Login />
+        </PersistGate>
+      </Provider>
+    </GluestackUIProvider>
   )
 }
 
--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
-import { Platform, Text, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { prueba } from "../../state/profile/profileSlice";
+import React from "react";
+import { AddIcon, Button, ButtonIcon, ButtonText, Center, HStack, Text } from "@gluestack-ui/themed";
 
 const Login = () => {
 
-    const dispatch = useDispatch()
-    const profile = useSelector((state) => state.profile)
-    console.log(Platform.OS, profile)
-
-    useEffect(() => {
-        //dispatch(prueba({ name: 'ACA ES ANDROID' }))
-    }, [])
-
     return (
-        <View>
-            <Text>qwdqw</Text>
-        </View>
+        <Center>
+            <HStack>
+
+                <Text color="red">
+                    hola
+                </Text>
+
+                <Button size="md" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} >
+                    <ButtonText>Add </ButtonText>
+                    <ButtonIcon as={AddIcon} />
+                </Button>
+            </HStack>
+
+        </Center>
     )
 }
 
